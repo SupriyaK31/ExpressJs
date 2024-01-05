967e393e6878a54cee18beb75fc259ce9949126c
SupriyaK31 <supriyakittur31@gmail.com>
2024-01-05 18:34:33 +0530

    express

---
+++ app1.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+const path=require('path');
+
+const app=express();
+app.use(bodyParser.urlencoded({ extended: true }));
+const adminRoute=require('./routes/admin');
+const shopRoute=require('./routes/shop');
+app.use(adminRoute);
+app.use(shopRoute);
+
+app.use((req,res,next)=>{
+res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+app.listen(3000);
