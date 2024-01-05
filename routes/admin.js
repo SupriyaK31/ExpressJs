399d416f8427223472cd9cb0ec5245cefcb3738b
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-01-05 21:32:40 +0530

    js

--- routes/admin.js
+++ routes/admin.js
@@ -11,4 +11,15 @@ router.post('/add-product',(req,res,next)=>{
   res.redirect('/');
  
 });
+router.get('/contactus',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','contactus.html'));
+ 
+});
+
+router.post('/success',(req,res,next)=>{
+    res.sendFile(path.join(rootDir,'views','success.html'));
+//     console.log('Form submitted successfully');
+//   res.redirect('/');
+ 
+});
 module.exports=router;
