288d745d9d60125571194f82e10fb0c4c840a8b2
SupriyaK31 <122157589+SupriyaK31@users.noreply.github.com>
2024-01-06 16:32:05 +0530

    controller.

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,18 @@
-const express=require('express');
 const path = require('path');
-const rootDir=require('../util/path');
-const router=express.Router();
-router.get('/add-product',(req,res,next)=>{
-  //  res.sendFile(path.join(__dirname,'../','views','add-product.html'));
-    res.sendFile(path.join(rootDir,'views','add-product.html'));
-});
-router.post('/add-product',(req,res,next)=>{
-    console.log(req.body);
-  res.redirect('/');
- 
-});
-router.get('/contactus',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','contactus.html'));
- 
-});
 
-router.post('/success',(req,res,next)=>{
-    res.sendFile(path.join(rootDir,'views','success.html'));
-//     console.log('Form submitted successfully');
-//   res.redirect('/');
- 
-});
-module.exports=router;
+const express = require('express');
+
+const controllerProduct=require('../controllers/products');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product',controllerProduct.getAddProduct);
+
+// /admin/add-product => POST
+router.post('/add-product', controllerProduct.postAddProduct);
+
+exports.routes = router;
+exports.products = products;
